20ce30bbd103696cf91cd1f128d381a3ff76f3ca
123Aang <20cse0173@vvce.ac.in>
2021-02-19 11:50:46 +0000

    adarsh

--- Program_2.c
+++ Program_2.c
@@ -5,10 +5,20 @@
 
 int main()
 {
-	char str[10];
+	char str[10] ,rev[10];
+  int beg , end , count=0;
 	scanf("%s", str);
 
 	//Write your code here
-
+ while(str[count]!='\0')
+ count++;
+ end=count-1 ;
+ for (beg=0; beg < count; beg++)
+ {
+   rev[beg]=str[end];
+   end--;
+ }
+rev[beg]= '\0';
+printf("%s\n", rev);
 	return 0;
 }
